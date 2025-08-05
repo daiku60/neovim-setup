0d5384ad15361597e95513c8acc896ae1d726556
Jordi Serra <jordi@theleftbit.com>
2025-08-05 21:58:43 +0200

    plugins setup

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require 'core.options'
 require 'core.keymaps'
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -13,33 +14,6 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 require('lazy').setup({
-{
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-  }
-},
-{
-'shaunsingh/nord.nvim',
-lazy = false,
-priority = 1000,
-config = function()
-	-- Example config in lua
-	vim.g.nord_contrast = true
-	vim.g.nord_borders = false
-	vim.g.nord_disable_background = false
-	vim.g.nord_italic = false
-	vim.g.nord_uniform_diff_background = true
-	vim.g.nord_bold = false
-
-	-- Load the colorscheme
-	require('nord').set()
-end
-}
+require 'plugins.neotree',
+require 'plugins.colortheme'
 })
