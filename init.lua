51e1fde1c0e8a774186a0e83550a29f9eace3eaa
Jordi Serra <jordi@theleftbit.com>
2025-08-15 10:46:07 +0200

    neoscroll and multiple themes
    added neoscroll to animate page up and down
    added bamboo theme
    removed theme defaults in plugins
    added bamboo as default in init

--- init.lua
+++ init.lua
@@ -15,7 +15,8 @@ rtp:prepend(lazypath)
 
 require('lazy').setup {
   require 'plugins.neotree',
-  require 'plugins.colortheme',
+  require 'plugins.themes.bamboo',
+  require 'plugins.themes.nord',
   require 'plugins.bufferline',
   require 'plugins.lualine',
   require 'plugins.treesitter',
@@ -29,5 +30,48 @@ require('lazy').setup {
   require 'plugins.misc',
   require 'plugins.debug',
   require 'plugins.lazygit',
-  require 'plugins.vimbegood',
+  require 'plugins.neoscroll',
 }
+
+vim.env.XDG_CONFIG_HOME = vim.fn.expand '$HOME/.config'
+
+-- require('nord').set()
+require('bamboo').load()
+
+-- adds custom anumation durations to keymaps for neoscroll
+neoscroll = require 'neoscroll'
+local keymap = {
+  ['<C-u>'] = function()
+    neoscroll.ctrl_u { duration = 250 }
+  end,
+  ['<C-d>'] = function()
+    neoscroll.ctrl_d { duration = 250 }
+  end,
+  ['<C-b>'] = function()
+    neoscroll.ctrl_b { duration = 450 }
+  end,
+  ['<C-f>'] = function()
+    neoscroll.ctrl_f { duration = 450 }
+  end,
+  ['<C-y>'] = function()
+    -- this scrolls the editor animated, but does not move the cursor to that position
+    neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+  end,
+  ['<C-e>'] = function()
+    -- this scrolls the editor animated, but does not move the cursor to that position
+    neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+  end,
+  ['zt'] = function()
+    neoscroll.zt { half_win_duration = 250 }
+  end,
+  ['zz'] = function()
+    neoscroll.zz { half_win_duration = 250 }
+  end,
+  ['zb'] = function()
+    neoscroll.zb { half_win_duration = 250 }
+  end,
+}
+local modes = { 'n', 'v', 'x' }
+for key, func in pairs(keymap) do
+  vim.keymap.set(modes, key, func)
+end
