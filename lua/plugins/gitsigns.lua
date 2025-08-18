a74aac1cd9be3910b2333d60732fbf9046c2050e
Jordi Serra <jordi@theleftbit.com>
2025-08-18 13:44:09 +0200

    formatting some luas

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,4 @@
--- Adds git related signs to the gutter, as well as utilities for managing changesº
+-- Adds git related signs to the gutter, as well as utilities for managing changes
 return {
   'lewis6991/gitsigns.nvim',
   opts = {
@@ -16,5 +16,27 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+
+    -- on_attach function to set keymaps per buffer
+    on_attach = function(bufnr)
+      local gs = require 'gitsigns'
+      local opts = { buffer = bufnr, remap = false }
+
+      -- Navigate to next hunk
+      vim.keymap.set('n', ']c', function()
+        if vim.wo.diff then
+          return ']c'
+        end
+        gs.next_hunk()
+      end, opts)
+
+      -- Navigate to previous hunk
+      vim.keymap.set('n', '[c', function()
+        if vim.wo.diff then
+          return '[c'
+        end
+        gs.prev_hunk()
+      end, opts)
+    end,
   },
 }
