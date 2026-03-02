3de5be6b60f445faa477e77be4e8482795710532
Jordi Serra <jordi@theleftbit.com>
2026-03-02 12:04:19 +0100

    telescope to show all

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -59,13 +59,13 @@ return {
       },
       pickers = {
         find_files = {
-          file_ignore_patterns = { 'node_modules', '%.git', '%.venv' },
+          file_ignore_patterns = { 'node_modules', '%.venv' }, -- removed '%.git'
           hidden = true,
         },
         live_grep = {
-          file_ignore_patterns = { 'node_modules', '%.git', '%.venv' },
+          file_ignore_patterns = { 'node_modules', '%.venv' }, -- removed '%.git'
           additional_args = function(_)
-            return { '--hidden' }
+            return { '--hidden', '--no-ignore', '--glob', '!.git/*' }
           end,
         },
       },
